2262b027fb0c87545f4c738ced355e8bf8cbb58a
soumyabrata <soumyabratam6@gmail.com>
2024-08-28 08:13:26 +0530

    all file added.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
+  const[count,setCount]= useState(0);
+  const handelOnclick = ()=>{
+    setCount(count+1);
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="container">
+        <p>Button clicked {count} times</p>
+        <button type="button" class="btn btn-secondary" onClick={handelOnclick}>Click me</button>
     </div>
   )
 }
